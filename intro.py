202eb77cfeb57b286e6939a6ea5c26ea576a52fc
pipevel <admin@lapapaya.org>
2025-03-10 06:18:41 -0500

    1. Intro.py

---
+++ intro.py
@@ -0,0 +1,48 @@
+import pycat
+
+datos = {
+    "actividad": "producción de petróleo",
+    "volumen": 10000, # barriles
+    "factor_emision": 0.43
+}
+
+
+
+
+print(dir(pycat))
+# Variables económicas
+
+Precio_venta = 0
+Costo = 0
+Almacenamiento = 0
+Precio_competencia = 0
+Combustibles = 0
+Consumo_economico = 0
+Costo_beneficio = 0
+Competitividad = 0
+
+# Variables normativas
+
+Estándares = 0
+
+# Variables ambientales
+
+Datos_ambientales = 0
+Huella_ecológica = 0
+Beneficios_ambientales = 0
+Emision_gases = 0
+
+# Variables de tipo texto
+
+Investigacion = "Investigación"
+Huella_carbono = "Huella de carbono"
+Impacto_ambiental = "Impacto"
+Herramientas_medicion = ["Herramienta1", "Herramienta2", "Herramienta3"]
+Analisis_financiero = ["Costo de funcionamiento", "Competitividad", "Proveedores", "Gastos mensuales", "Transformaciones", "Estrategias", "Indicadores de rendimiento"]
+Herramientas_medicion_py = {"Python Climate Analytics Toolkit (PyCAT):": "Funcionalidades: Ofrece módulos para calcular emisiones de gases de efecto invernadero basados en diferentes actividades industriales, incluyendo el sector de hidrocarburos. Facilita la integración de datos específicos de la empresa para obtener estimaciones precisas.",
+                            "CarboScope:": "Funcionalidades: Permite modelar y visualizar las emisiones de carbono a lo largo de la cadena de suministro. Incluye herramientas para identificar áreas de mejora en la reducción de emisiones y generar reportes personalizados.",
+                            "CO2Calc-Py:": "Funcionalidades: Calculadora de emisiones de CO₂ adaptable a distintos procesos industriales. Ofrece la posibilidad de simular escenarios y evaluar el impacto de medidas de mitigación."}
+
+
+
+print(Herramientas_medicion_py)
